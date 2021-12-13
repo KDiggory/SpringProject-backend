674ebbf200124a89eec5ce7ee9d9c03d6d8770f2
Katie Diggory <Katie.diggory@gmail.com>
2021-12-13 10:23:57 +0000

    GAR-27 #comment Adding month not found exception

---
+++ src/main/java/com/bae/backend/exceptions/MonthNotFoundException.java
@@ -0,0 +1,42 @@
+package com.bae.backend.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code= HttpStatus.NOT_FOUND, reason="No plant found with that planting month")
+public class MonthNotFoundException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -6281720380638927392L;
+
+	public MonthNotFoundException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public MonthNotFoundException(String message, Throwable cause, boolean enableSuppression,
+			boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+	public MonthNotFoundException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public MonthNotFoundException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public MonthNotFoundException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+
+}
+
