168b3b9f607a8de81918c10c6dba60b38f9dd441
Katie Diggory <Katie.diggory@gmail.com>
2021-12-13 11:39:53 +0000

    Controller integration testing complete

--- src/test/java/com/bae/backend/controller/PlantsControllerTest.java
+++ src/test/java/com/bae/backend/controller/PlantsControllerTest.java
@@ -1,7 +1,9 @@
 package com.bae.backend.controller;
 
-
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -25,65 +27,110 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc // sets up the mockmvc object
-@Sql(scripts = {"classpath:plant-schema.sql", "classpath:plant-data.sql"}, executionPhase = ExecutionPhase.BEFORE_TEST_METHOD)
+@Sql(scripts = { "classpath:plant-schema.sql",
+		"classpath:plant-data.sql" }, executionPhase = ExecutionPhase.BEFORE_TEST_METHOD)
 @ActiveProfiles("test")
 public class PlantsControllerTest {
-	
+
 	@Autowired
-	private MockMvc mvc; 
-	
+	private MockMvc mvc;
+
 	@Autowired
 	private ObjectMapper mapper;
-	
-	
-	@Test 
+
+	@Test
 	void testCreate() throws Exception {
-		Plants plant = new Plants( "Daffodil", "green", "October", "sun","Yellow");
+		Plants plant = new Plants("Daffodil", "green", "October", "sun", "Yellow");
 		String plantAsJSON = this.mapper.writeValueAsString(plant);
-		
+
 		RequestBuilder req = post("/createPlant").contentType(MediaType.APPLICATION_JSON).content(plantAsJSON);
-		
-		Plants testPlant = new Plants(4, "Daffodil", "green", "October", "sun","Yellow");
-		String testPlantAsJSON = this.mapper.writeValueAsString(testPlant);		
-		
+
+		Plants testPlant = new Plants(4, "Daffodil", "green", "October", "sun", "Yellow");
+		String testPlantAsJSON = this.mapper.writeValueAsString(testPlant);
+
 		ResultMatcher checkStatus = status().isCreated();
 		ResultMatcher checkBody = content().json(testPlantAsJSON);
-		
+
 		this.mvc.perform(req).andExpect(checkStatus).andExpect(checkBody);
 	}
-	
+
 	@Test
 	void testGetAll() throws Exception {
-		List<Plants> plant = List.of(new Plants(1, "Daffodil", "green", "October", "sun","Yellow"),
-				new Plants(2, "Peony", "green", "October", "sun","Pink"),
-				new Plants(3, "Marigold", "green", "March", "sun","Orange"));
+		List<Plants> plant = List.of(new Plants(1, "Daffodil", "green", "October", "sun", "Yellow"),
+				new Plants(2, "Peony", "green", "October", "sun", "Pink"),
+				new Plants(3, "Marigold", "green", "March", "sun", "Orange"));
 		String plantAsJSON = this.mapper.writeValueAsString(plant);
-		
-		RequestBuilder req = post("/getAll");
-			
-		ResultMatcher checkStatus = status().isCreated();
+
+		RequestBuilder req = get("/getAll");
+
+		ResultMatcher checkStatus = status().isOk();
 		ResultMatcher checkBody = content().json(plantAsJSON);
-		
+
 		this.mvc.perform(req).andExpect(checkStatus).andExpect(checkBody);
-		
-		
 	}
-	
 
-//	@GetMapping("getAll")
-//	
-//	
-//	@GetMapping("/getPlantById/{id}")
-//	
-//	
-//	@GetMapping("/getPlantByName/{name}")
-//	
-//	@GetMapping("/getPlantByMonth/{month}")
-//	
-//	
-//	@PutMapping("/updatePlant/{id}") 
-//	
-//	@DeleteMapping("/deletePlant/{id}") 
-	
+	@Test
+	void testGetById() throws Exception {
+		Plants plant = new Plants(1, "Daffodil", "green", "October", "sun", "Yellow");
+		String plantAsJSON = this.mapper.writeValueAsString(plant);
 
+		RequestBuilder req = get("/getPlantById/1");
+
+		ResultMatcher checkStatus = status().isOk();
+		ResultMatcher checkBody = content().json(plantAsJSON);
+
+		this.mvc.perform(req).andExpect(checkStatus).andExpect(checkBody);
+	}
+
+	@Test
+	void testGetByName() throws Exception {
+		Plants plant = new Plants(1, "Daffodil", "green", "October", "sun", "Yellow");
+		String plantAsJSON = this.mapper.writeValueAsString(plant);
+
+		RequestBuilder req = get("/getPlantByName/Daffodil");
+
+		ResultMatcher checkStatus = status().isOk();
+		ResultMatcher checkBody = content().json(plantAsJSON);
+
+		this.mvc.perform(req).andExpect(checkStatus).andExpect(checkBody);
+
+	}
+
+	@Test
+	void testGetByMonth() throws Exception {
+		List<Plants> plant = List.of(new Plants(1, "Daffodil", "green", "October", "sun", "Yellow"),
+				new Plants(2, "Peony", "green", "October", "sun", "Pink"));
+		String plantAsJSON = this.mapper.writeValueAsString(plant);
+
+		RequestBuilder req = get("/getPlantByMonth/October");
+
+		ResultMatcher checkStatus = status().isOk();
+		ResultMatcher checkBody = content().json(plantAsJSON);
+
+		this.mvc.perform(req).andExpect(checkStatus).andExpect(checkBody);
+
+	}
+
+	@Test
+	void testUpdatePlant() throws Exception {
+		Plants plant = new Plants(1, "Narcissus", "green", "October", "sun", "Yellow and Orange");
+		String plantAsJSON = this.mapper.writeValueAsString(plant);
+
+		RequestBuilder req = put("/updatePlant/1").contentType(MediaType.APPLICATION_JSON).content(plantAsJSON);
+
+		ResultMatcher checkStatus = status().isAccepted();
+		ResultMatcher checkBody = content().json(plantAsJSON);
+
+		this.mvc.perform(req).andExpect(checkStatus).andExpect(checkBody);
+
+	}
+
+	@Test
+	void testDelete() throws Exception {
+		RequestBuilder req = delete("/deletePlant/1").contentType(MediaType.APPLICATION_JSON);
+		ResultMatcher checkStatusGet = status().isNoContent();
+
+		this.mvc.perform(req).andExpect(checkStatusGet);
+
+	}
 }
